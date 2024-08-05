cbc5c5a69a61f946043856dd6f7c44d38e7faa6c
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-05 15:22:35 +0100

    architecture projet spring boot

---
+++ ITsupportApp_BACKEND/src/main/java/com/itsupport/itsupport_backend/ITsupportBackendApplication.java
@@ -0,0 +1,13 @@
+package com.itsupport.itsupport_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITsupportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITsupportBackendApplication.class, args);
+	}
+
+}
---
+++ ITsupportApp_BACKEND/src/test/java/com/itsupport/itsupport_backend/ITsupportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsupport.itsupport_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITsupportBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
