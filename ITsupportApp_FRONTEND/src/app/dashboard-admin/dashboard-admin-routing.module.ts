bf225fe6e3185e1b4b6d7bb871e28072c3d5c24d
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-12 14:41:05 +0100

    modifier dashboard à utiliser les Component

--- ITsupportApp_FRONTEND/src/app/dashboard-admin/dashboard-admin-routing.module.ts
+++ ITsupportApp_FRONTEND/src/app/dashboard-admin/dashboard-admin-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardAdminComponent } from './dashboard-admin.component';
+import {TechnicienComponent} from "./technicien/technicien.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardAdminComponent
+  },
+  {
+    path: 'Technicien',
+    component: TechnicienComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- ITsupportApp_FRONTEND/src/app/dashboard-admin/dashboard-admin.module.ts
+++ ITsupportApp_FRONTEND/src/app/dashboard-admin/dashboard-admin.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardAdminRoutingModule } from './dashboard-admin-routing.module';
+import {TechnicienComponent} from "./technicien/technicien.component";
+import {DashboardAdminComponent} from "./dashboard-admin.component";
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { StatistiqueComponent } from './statistique/statistique.component';
+import { ListTechnicienComponent } from './technicien/list-technicien/list-technicien.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    DashboardAdminRoutingModule
-  ]
+  declarations: [
+    DashboardAdminComponent,
+    TechnicienComponent,
+    NavbarComponent,
+    SidebarComponent,
+    StatistiqueComponent,
+    ListTechnicienComponent
+  ],
+  exports: [
+    DashboardAdminComponent,
+    TechnicienComponent
+  ],
+    imports: [
+        CommonModule,
+        DashboardAdminRoutingModule,
+        FormsModule,
+    ]
 })
 export class DashboardAdminModule { }
