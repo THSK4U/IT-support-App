e3c19f1afdd899ee1739d299427f96cc10b6fcda
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-12 02:18:23 +0100

    ajouter routing sur Admin

--- ITsupportApp_FRONTEND/src/app/app-routing.module.ts
+++ ITsupportApp_FRONTEND/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TechnicienComponent} from "./dashboard-admin/technicien/technicien.component";
+import {DashboardAdminComponent} from "./dashboard-admin/dashboard-admin.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Admin',
+    loadChildren: () => import('./dashboard-admin/dashboard-admin.module').then(m => m.DashboardAdminModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
