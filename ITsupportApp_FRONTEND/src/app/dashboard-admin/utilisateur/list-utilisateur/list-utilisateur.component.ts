7b9ec62287df6726aa07420ad17da664bbac80fa
TAHA SELLAK <sellaktaha1@gmail.com>
2024-08-12 22:43:55 +0100

    Afficher List Utilisateurs

---
+++ ITsupportApp_FRONTEND/src/app/dashboard-admin/utilisateur/list-utilisateur/list-utilisateur.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-utilisateur',
+  templateUrl: './list-utilisateur.component.html',
+  styleUrls: ['./list-utilisateur.component.css']
+})
+export class ListUtilisateurComponent {
+
+}
